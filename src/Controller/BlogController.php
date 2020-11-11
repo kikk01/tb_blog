83ffb24a0c233157bec46e66a8c14f3b8a9707ae
kikk01 <hugobar@live.fr>
2020-11-11 11:58:32 +0100

    add comment form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,12 +23,23 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/post/{id}", name="blog_read")
-     *
+     * @Route("/article-{id}", name="blog_read")
+     * @param Post $post
      * @return Response
      */
-    public function read(Post $post) : Response
+    public function read(Post $post, Request $request) : Response
     {
-        return $this->render('read.html.twig', ['post' => $post]);
+        $comment = new Comment();
+        $comment->setPost($post);
+        $form = $this->createForm(CommentType::class, $comment)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($comment);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute("blog_read", ["id" => $post->getId()]);
+        }
+        return $this->render('read.html.twig', [
+            'post' => $post,
+            "form" => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class CommentType
+ * @package App\Form
+ */
+class CommentType extends AbstractType
+{
+    /**
+     * @inheritdoc
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("author", TextType::class, [
+                "label" => "Pseudo :"
+            ])
+            ->add("content", TextareaType::class, [
+                "label" => "Votre message"
+            ])
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param OptionsResolver $resolver
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault("data_class", Comment::class);
+    }
+
+
+
+}
