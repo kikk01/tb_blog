baecfb336b34469f90658e28d8ba30ddbfb59dce
kikk01 <hugobar@live.fr>
2021-01-29 16:41:48 +0100

    delete abastractController from controllers

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,25 +8,36 @@ use App\Entity\Post;
 use App\Handler\CommentHandler;
 use App\Handler\PostHandler;
 use App\Security\Voter\PostVoter;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
 
-class BlogController extends AbstractController
+class BlogController
 {
+    use AuthorizationTrait;
+
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+
     /**
      * @Route("/", name="blog")
-     * @param Request $request
-     * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $limit = $request->get("limit", 10);
         $page = $request->get("page", 1);
 
         /** @var Paginator $posts */
-        $posts = $this->getDoctrine()->getManager()->getRepository(Post::class)->getPaginatedPosts(
+        $posts = $entityManager->getRepository(Post::class)->getPaginatedPosts(
             $page,
             $limit
         );
@@ -37,13 +48,13 @@ class BlogController extends AbstractController
             min($page + 3, $pages)
         );
 
-        return $this->render('blog/index.html.twig', [
+        return new Response($this->twig->render('blog/index.html.twig', [
             'posts' => $posts,
             'pages' => $pages,
             'page' => $page,
             'limit' => $limit,
             'range' => $range
-        ]);
+        ]));
     }
 
     /**
@@ -53,71 +64,70 @@ class BlogController extends AbstractController
      * @param CommentHandler $commentHandler
      * @return Response
      */
-    public function read(Post $post, Request $request, CommentHandler $commentHandler) : Response
-    {
+    public function read(
+        Post $post,
+        Request $request,
+        CommentHandler $commentHandler,
+        UrlGeneratorInterface $urlGenerator
+    ) : Response {
+
         $comment = new Comment();
         $comment->setPost($post);
 
         $options = ["validation_groups" => $this->isGranted("ROLE_USER") ? "Default" : ["Default", "anonymous"]];
 
         if ($commentHandler->handle($request, $comment, $options)) {
-            return $this->redirectToRoute("blog_read", ["id" => $post->getId()]);
+            return new RedirectResponse($urlGenerator->generate("blog_read", ["id" => $post->getId()]));
         }
 
-        return $this->render('blog/read.html.twig', [
+        return new Response($this->twig->render('blog/read.html.twig', [
             'post' => $post,
             "form" => $commentHandler->createView()
-        ]);
+        ]));
     }
 
     /**
      * @Route("/publier-article", name="blog_create")
-     * @param Request $request
-     * @param PostHandler $postHandler
-     * @return Response
      */
     public function create(
         Request $request,
-        PostHandler $postHandler
+        PostHandler $postHandler,
+        UrlGeneratorInterface $urlGenerator
     ) : Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         $post = new Post();
-        $post->setUser($this->getUser());
 
         $options = [
             'validation_groups' => ['Default' => 'create']
         ];
 
         if ($postHandler->handle($request, $post, $options)) {
-            return $this->redirectToRoute("blog_read", ["id" => $post->getId()]);
+            return new RedirectResponse($urlGenerator->generate("blog_read", ["id" => $post->getId()]));
         }
 
-        return $this->render('blog/create.html.twig', [
+        return new Response($this->twig->render('blog/create.html.twig', [
             'form' => $postHandler->createView()
-        ]);
+        ]));
     }
 
     /**
      * @Route("/modifier-article/{id}", name="blog_update")
-     * @param Request $request
-     * @param Post $post
-     * @param PostHandler $postHandler
-     * @return Response
      */
     public function update(
         Post $post,
         Request $request,
-        PostHandler $postHandler
+        PostHandler $postHandler,
+        UrlGeneratorInterface $urlGenerator
     ) : Response {
         $this->denyAccessUnlessGranted(PostVoter::EDIT, $post);
 
         if ($postHandler->handle($request, $post)) {
-            return $this->redirectToRoute("blog_read", ["id" => $post->getId()]);
+            return new RedirectResponse($urlGenerator->generate("blog_read", ["id" => $post->getId()]));
         }
 
-        return $this->render('blog/update.html.twig', [
+        return new Response($this->twig->render('blog/update.html.twig', [
             'form' => $postHandler->createView()
-        ]);
+        ]));
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,30 +6,35 @@ use App\DataTransferObject\Credentials;
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Twig\Environment;
 
 /**
  * Class SecurityController
  * @package App\Controller
  */
-class SecurityController extends AbstractController
+class SecurityController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        $form = $this->createForm(LoginType::class, new Credentials($authenticationUtils->getLastUsername()));
+    public function login(
+        AuthenticationUtils $authenticationUtils,
+        FormFactoryInterface $formFactory,
+        Environment $twig
+    ): Response {
+        $form = $formFactory->create(LoginType::class, new Credentials($authenticationUtils->getLastUsername()));
 
         if (null !== $authenticationUtils->getLastAuthenticationError(false)) {
             $form->addError(new FormError($authenticationUtils->getLastAuthenticationError()->getMessage()));
         }
         
-        return $this->render('security/login.html.twig', [
+        return new Response($twig->render('security/login.html.twig', [
             'form' => $form->createView()
-        ]);
+        ]));
     }
 
         /**
